0badf77ad3dadfca00429cd836e741125bea5a24
Jiyang Ye <yejiyang@gmail.com>
2023-11-27 22:53:39 +0100

    fix point features

--- src/examples/SelectFeature.tsx
+++ src/examples/SelectFeature.tsx
@@ -9,155 +9,65 @@ import Select from "ol/interaction/Select.js";
 import Feature from "ol/Feature.js";
 import Point from "ol/geom/Point.js";
 import { Icon, Style, RegularShape } from "ol/style.js";
+import CircleStyle from "ol/style/Circle";
+import { Cluster } from "ol/source";
+import { fromLonLat } from "ol/proj";
+
+const coordinates = [
+  [0, 0],
+  [1, 1],
+  [2, 2],
+  [3, 3],
+  [0, 1],
+  [1, 2],
+  [2, 3],
+  [3, 0],
+  [0, 2],
+  [1, 3],
+  [2, 0],
+  [3, 1],
+].map((coordinate) => fromLonLat(coordinate));
+
+const styleFunction = (feature) => {
+  return new Style({
+    geometry: feature.getGeometry(),
+    image: new CircleStyle({
+      radius: 10,
+      fill: new Fill({
+        color: "rgba(255, 153, 0, 0.8)",
+      }),
+    }),
+  });
+};
+
+const pointsLayer = new VectorLayer({
+  source: new Cluster({
+    distance: 1,
+    source: new VectorSource({
+      features: coordinates.map((coordinate) => {
+        return new Feature({
+          geometry: new Point(coordinate),
+        });
+      }),
+    }),
+  }),
+  style: styleFunction,
+  zIndex: 10,
+});
 
 const SelectFeature = () => {
   const { map } = useContext(MapContext);
 
-  //   const selected = new Style({
-  //     fill: new Fill({
-  //       color: "#eeeeee",
-  //     }),
-  //     stroke: new Stroke({
-  //       color: "rgba(255, 255, 255, 0.7)",
-  //       width: 2,
-  //     }),
-  //   });
-
-  //   function selectStyle(feature: any) {
-  //     const color = feature.get("COLOR") || "#eeeeee";
-  //     selected.getFill().setColor(color);
-  //     return selected;
-  //   }
-
-  //   // select interaction working on "click"
-  //   const selectClick = new Select({
-  //     condition: "click",
-  //     style: selectStyle,
-  //   });
-
-  //   const highlightStyle = new Style({
-  //     fill: new Fill({
-  //       color: "#EEE",
-  //     }),
-  //     stroke: new Stroke({
-  //       color: "#3399CC",
-  //       width: 2,
-  //     }),
-  //   });
-
-  const iconFeature = new Feature({
-    geometry: new Point([0, 0]),
-    name: "Null Island",
-    population: 4000,
-    rainfall: 500,
-  });
-
-  const iconStyle = new Style({
-    geometry: iconFeature.getGeometry(),
-    image: new RegularShape({
-      radius1: 10,
-      radius2: 3,
-      points: 5,
-      angle: Math.PI,
-      fill: new Fill({
-        color: "rgba(255, 153, 0, 0.8)",
-      }),
-      stroke: new Stroke({
-        color: "rgba(255, 204, 0, 0.2)",
-        width: 1,
-      }),
-    }),
-  });
-
-  iconFeature.setStyle(iconStyle);
-
-  const iconFeature2 = new Feature({
-    geometry: new Point([5, 5]),
-    name: "Null Island 2",
-    population: 4000,
-    rainfall: 500,
-  });
-
-  const iconStyle2 = new Style({
-    geometry: iconFeature2.getGeometry(),
-    image: new RegularShape({
-      radius1: 10,
-      radius2: 3,
-      points: 5,
-      angle: Math.PI,
-      fill: new Fill({
-        color: "rgba(255, 153, 0, 0.8)",
-      }),
-      stroke: new Stroke({
-        color: "rgba(255, 204, 0, 0.2)",
-        width: 1,
-      }),
-    }),
-  });
-
-  iconFeature2.setStyle(iconStyle2);
-
-  //   const getFeatureStyle = (feature: any) => {
-  //     return new Style({
-  //       geometry: iconFeature.getGeometry(),
-  //       image: new RegularShape({
-  //         radius1: 10,
-  //         radius2: 3,
-  //         points: 5,
-  //         angle: Math.PI,
-  //         fill: new Fill({
-  //           color: "rgba(255, 153, 0, 0.8)",
-  //         }),
-  //         stroke: new Stroke({
-  //           color: "rgba(255, 204, 0, 0.2)",
-  //           width: 1,
-  //         }),
-  //       }),
-  //     });
-  //   };
-
-  //   const generateDummyIconFeatures = () => {
-  //     const features = [];
-  //     for (let i = 0; i < 100; i++) {
-  //       // Generate random coordinates within the specified range
-  //       const longitude = Math.random() * 10; // Longitude between 0 and 10
-  //       const latitude = Math.random() * 10; // Latitude between 0 and 10
-
-  //       console.log(longitude, latitude);
-
-  //       const iconFeature = new Feature({
-  //         geometry: new Point([longitude, latitude]),
-  //         name: `Dummy Location ${i}`,
-  //         population: 4000, // Random population
-  //         rainfall: 500, // Random rainfall
-  //       });
-
-  //       iconFeature.setStyle(iconStyle);
-
-  //       features.push(iconFeature);
-  //     }
-  //     return features;
-  //   };
-
-  const vector = new VectorLayer({
-    // background: "blue",
-    source: new VectorSource({
-      features: [iconFeature2, iconFeature],
-    }),
-    zIndex: 10,
-    opacity: 1,
-  });
-
-  vector.set("name", "VectorLayer");
+  pointsLayer.set("name", "PointsLayer");
 
   useEffect(() => {
     if (!map) return;
 
-    map.addLayer(vector);
+    map.addLayer(pointsLayer);
 
     return () => {
       if (map) {
-        map.removeLayer(vector);
+        map.removeLayer(pointsLayer);
       }
     };
   }, [map]);
